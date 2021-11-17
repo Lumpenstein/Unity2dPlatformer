17831fcb7e4dc99dad4c92db29e384740d8000aa
Lumpenstein <jhangchaussy@gmail.com>
2021-11-17 19:27:41 +0100

    Added endscreen

---
+++ Assets/Scripts/EndScreenMenu.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreenMenu : MonoBehaviour
+{
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
